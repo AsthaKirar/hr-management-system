c683094542588d8761460fb66910b65de2a142f0
AsthaKirar <asthap602@gmail.com>
2024-10-18 22:00:45 +0530

    commit

---
+++ src/models/Payroll.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const payrollSchema = new mongoose.Schema({
+  employee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Employee',
+    required: true
+  },
+  month: {
+    type: Date,
+  },
+  daysWorked: {
+    type: Number,
+  },
+  baseSalary: {
+    type: Number,
+    required: true
+  },
+  overtimePay: {
+    type: Number,
+    default: 0
+  },
+  taxAmount: {
+    type: Number,
+    required: true
+  },
+  netSalary: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Payroll', payrollSchema);
